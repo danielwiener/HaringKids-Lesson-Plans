eb297e1caa40e3384b196a1d09b429069d99c25a
danielwiener <daniel@danielwiener.com>
2010-07-17 17:29:21 -0400

    changed Lessons by Title from a simple list to a TOC page with title, thumbnail and description

--- page_all_posts.php
+++ page_all_posts.php
@@ -21,9 +21,9 @@ Template Name: All Posts
 		<div id="container">
 			<div id="content" role="main">
 
-<h2>Lessons by Title</h2>
+<h1 class="page-title">Lessons by Title</h1>
 
-<ul>
+
 <?php
 /*
 global $wp_query;
@@ -38,11 +38,33 @@ $wp_query->query('caller_get_posts=1&orderby=post_date&order=DESC&posts_per_page
 
 /* foreach($myposts as $post) : */
 while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+<div id="post-<?php the_ID(); ?>" <?php post_class() ?>>
+			<h2 class="toc_title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyten' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
 
-<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li> 
-<?php /* endforeach; */ ?>
+
+			<div class="entry-summary">
+			<?php if(has_post_thumbnail()): ?>
+			<a href="<?php the_permalink(); ?>">
+			<?php the_post_thumbnail('small-thumbnail',  array('class' => 'alignleft', 'title' => trim(strip_tags($post->post_title)), 'alt' => trim(strip_tags($post->post_title)))); 
+
+			?>
+			</a>
+			<?php endif; ?>
+			
+			<?php
+					if(get_post_meta($post->ID, "_dwdescription_value", $single = true) != "") : ?>
+					<p><?php echo get_post_meta($post->ID, "_dwdescription_value", $single = true);?></p>
+								
+					<?php endif; ?>
+				
+			</div><!-- .entry-summary -->
+			
+			
+			<div class="entry-utility">
+			</div><!-- .entry-utility -->
+		</div><!-- #post-## -->
 <?php endwhile; ?>
-</ul>
+
 <?php numeric_pagination(); ?>
 			</div><!-- #content -->
 		</div><!-- #container -->
