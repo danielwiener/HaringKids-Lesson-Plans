4d506d9ab2eb3021b8fd1e3b6a0213f9b0abcbf4
danielwiener <daniel@danielwiener.com>
2010-07-18 16:17:30 -0400

    added a function to search the custom write panels... and only posts, not pages

--- functions.php
+++ functions.php
@@ -370,4 +370,83 @@ echo ('<span id="footer-thankyou">' . __('Dashboard modifications by <a href="ht
 }
 
 add_action('admin_footer_text', 'custom_footer');
+
+
+// search filter
+
+
+function custom_search_join($join) {
+    if ( is_search() && isset($_GET['s'])) {
+        global $wpdb;
+
+       $join = " LEFT JOIN $wpdb->postmeta ON $wpdb->posts.ID = $wpdb->postmeta.post_id ";
+    }
+    return($join);
+}
+add_filter('posts_join', 'custom_search_join');
+
+function custom_search_groupby($groupby) {
+    if ( is_search() && isset($_GET['s'])) {
+        global $wpdb;
+        $groupby = " $wpdb->posts.ID ";
+    }
+    return($groupby);
+}
+add_filter('posts_groupby', 'custom_search_groupby');
+
+function custom_search_where($where) {
+    $old_where = $where;
+    if (is_search() && isset($_GET['s'])) {
+        global $wpdb;
+        $customs = Array('_dwdescription_value', '_objective_value', '_materials_value', '_procedure_value', '_questions_value', '_resources_value', '_extensions_value');
+        $query = '';
+        $var_q = stripslashes($_GET['s']);
+        if ($_GET['sentence']) {
+            $search_terms = array($var_q);
+        }
+        else {
+            preg_match_all('/".*?("|$)|((?<=[\\s",+])|^)[^\\s",+]+/', $var_q, $matches);
+            $search_terms = array_map(create_function('$a', 'return trim($a, "\\"\'\\n\\r ");'), $matches[0]);
+        }
+        $n = ($_GET['exact']) ? '' : '%';
+        $searchand = '';
+        foreach((array)$search_terms as $term) {
+            $term = addslashes_gpc($term);
+            $query .= "{$searchand}(";
+                        $query .= "($wpdb->posts.post_title LIKE '{$n}{$term}{$n}')";
+            $query .= " OR ($wpdb->posts.post_content LIKE '{$n}{$term}{$n}')";
+            foreach($customs as $custom) {
+                $query .= " OR (";
+                $query .= "($wpdb->postmeta.meta_key = '$custom')";
+                $query .= " AND ($wpdb->postmeta.meta_value  LIKE '{$n}{$term}{$n}')";
+                $query .= ")";
+            }
+            $query .= ")";
+            $searchand = ' AND ';
+        }
+        $term = $wpdb->escape($var_q);
+        if (!$_GET['sentense'] && Count($search_terms) > 1 && $search_terms[0] != $var_q) {
+            $search .= " OR ($wpdb->posts.post_title LIKE '{$n}{$term}{$n}')";
+            $search .= " OR ($wpdb->posts.post_content LIKE '{$n}{$term}{$n}')";
+        }
+
+        if (!empty($query)) {
+            $where = " AND ({$query}) AND ($wpdb->posts.post_status = 'publish')  AND ($wpdb->posts.post_type = 'post') ";
+        }
+    }
+
+    return($where);
+}
+add_filter('posts_where', 'custom_search_where');
+
+/*
+function my_search_filter($query) {
+   if ($query->is_search) {
+      $query->set('post_type','post');
+   }
+   return $query;
+}
+add_filter('pre_get_posts','my_search_filter');
+*/
+
 ?>
