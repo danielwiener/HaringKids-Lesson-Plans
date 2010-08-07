106f215009eaea1a4b6ded9e3467aed5601f4bbf
danielwiener <daniel@danielwiener.com>
2010-08-07 17:39:29 -0400

    add author image, research on emailing authors through a form

--- author.php
+++ author.php
@@ -30,12 +30,16 @@ get_header(); ?>
 // If a user has filled out their description, show a bio on their entries.
 if ( get_the_author_meta( 'description' ) ) : ?>
 					<div id="entry-author-info">
+						<?php if (the_author_image()): ?>
 						<div id="author-avatar">
-							<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyten_author_bio_avatar_size', 60 ) ); ?>
+							<?php the_author_image(); ?> 
 						</div><!-- #author-avatar -->
+						<?php endif ?>
 						<div id="author-description">
 							<h2><?php printf( __( 'About %s', 'twentyten' ), get_the_author() ); ?></h2>
 							<?php the_author_meta( 'description' ); ?>
+							<?php $author_stuff = get_the_author_meta('user_email', get_the_author_meta( 'ID' ));
+							// echo '<pre>' .var_dump($author_stuff) . '</pre>'; ?>
 						</div><!-- #author-description	-->
 					</div><!-- #entry-author-info -->
 <?php endif; ?>
