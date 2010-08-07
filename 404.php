6cc73e38d85c3426a54b0afc95b51a219132190a
danielwiener <daniel@danielwiener.com>
2010-08-07 14:55:50 -0400

    test 404 when doing pagination problem, once fixed add advanced search.

---
+++ 404.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage Twenty_Ten
+ * @since Twenty Ten 1.0
+ */
+
+get_header(); ?>
+
+	<div id="container">
+		<div id="content" role="main">
+
+			<div id="post-0" class="post error404 not-found">
+				<h1 class="entry-title"><?php _e( 'Not Found', 'twentyten' ); ?></h1>
+				<div class="entry-content">
+				
+					<p><?php _e( 'Apologies, but the page you requested could not be found. Perhaps searching will help.', 'twentyten' ); ?></p>
+					<?php get_search_form(); ?>
+				
+				</div><!-- .entry-content -->
+				<hr />
+					<h2>Or try searching multiple categories</h2>
+					<p>Check categories below then click "Search Multiple Categories"</p>
+					<div class="entry-summary">
+					<?php include(STYLESHEETPATH . '/includes/advanced_search_form.php'); ?>
+					</div>
+			</div><!-- #post-0 -->
+
+		</div><!-- #content -->
+	</div><!-- #container -->
+	<script type="text/javascript">
+		// focus on search field after it has loaded
+		document.getElementById('s') && document.getElementById('s').focus();
+	</script>
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
