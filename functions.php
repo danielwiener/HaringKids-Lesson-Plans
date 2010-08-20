8fca6b0a44818545813ddd6f7eed1ece10125b88
danielwiener <daniel@danielwiener.com>
2010-08-20 16:00:31 -0400

    added google analytics to footer and commented out search filter

--- functions.php
+++ functions.php
@@ -492,6 +492,25 @@ function custom_search_where($where) {
 }
 add_filter('posts_where', 'custom_search_where');
 
+// add google analytics to footer
+function add_google_analytics() {
+echo '<script type="text/javascript">';
+echo "\n";
+echo '  var _gaq = _gaq || [];';
+echo '  _gaq.push(["_setAccount", "UA-17868145-1"]);';
+echo '  _gaq.push(["_trackPageview"]);';
+echo "\n";
+echo '  (function() {';
+echo '    var ga = document.createElement("script"); ga.type = "text/javascript"; ga.async = true;';
+echo '    ga.src = ("https:" == document.location.protocol ? "https://ssl" : "http://www") + ".google-analytics.com/ga.js";';
+echo '    var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ga, s);';
+echo '  })();';
+echo "\n";
+echo '</script>';
+}
+add_action('wp_footer', 'add_google_analytics');
+
+
 /*
 function my_search_filter($query) {
    if ($query->is_search) {
